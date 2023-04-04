3387d58af0a72f9f7b2b5e3d733ef85be083d2e8
cpa02 <cpa02@DESKTOP-001F323>
2023-04-04 08:00:41 -0500

    Agregar archivos de proyecto.

---
+++ EJERCICIO4/Controllers/E5Controller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EJERCICIO4.Controllers
+{
+    public class E5Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ EJERCICIO4/wwwroot/js/procesar.js
@@ -0,0 +1,16 @@
+﻿
+var hor = document.getElementById("horas");
+var tarifa = document.getElementById("tarifaH");
+
+function getOperacion() {
+
+    var Sueldo_bruto = (parseFloat(tarifa.value) * parseInt(hor.value)).toFixed(2);
+    var Importe_Descuento = (0.15 * Sueldo_bruto).toFixed(2);
+    var Sueldo_neto = Sueldo_bruto - Importe_Descuento;
+
+    document.getElementById("resultados").value = "Sueldo bruto: " + Sueldo_bruto + "\n" +
+        "Importe del descuento: " + Importe_Descuento + "\n" +
+        "Sueldo neto: " + Sueldo_neto;
+
+    return false;
+}
